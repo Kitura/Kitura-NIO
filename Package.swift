9a453bc1c72c87d3f534f7e707351f5078a497d2
Pushkar N Kulkarni <pushkar.nk@in.ibm.com>
2018-04-12 12:20:56 +0530

    Fix the swift-nio level to 1.3.1
    We are seeing an assert failure with 1.4.1
    https://github.com/apple/swift-nio/issues/302

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "1.4.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", .exact("1.3.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
