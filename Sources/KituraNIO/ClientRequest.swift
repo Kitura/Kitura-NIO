baaf0f09dc5b5b926941b26fdea23ccdd67cf326
Nethra Ravindran <nethra.ravindran@in.ibm.com>
2018-04-22 10:30:23 +0530

    Add support for HTTPS client

--- Sources/KituraNIO/ClientRequest.swift
+++ Sources/KituraNIO/ClientRequest.swift
@@ -1,7 +1,7 @@
 import NIO
 import NIOHTTP1
 import Foundation
-
+import NIOOpenSSL
 
 public class ClientRequest {
     
@@ -15,7 +15,7 @@ public class ClientRequest {
 
     public private(set) var password: String?
 
-    public private(set) var maxRedirects = 10
+    public internal(set) var maxRedirects = 10
 
     public private(set) var closeConnection = false
 
@@ -27,8 +27,20 @@ public class ClientRequest {
 
     var hostName: String?
 
-    /// Should SSL verification be disabled
-    private var disableSSLVerification = false
+    /// Should SSL verification be enabled
+    private var enableSSLVerification = false {
+        didSet {
+            if enableSSLVerification {
+                self.sslConfig = TLSConfiguration.forClient(certificateVerification: .none)
+            }
+        }
+    }
+
+    var sslConfig: TLSConfiguration?
+
+    internal var redirectCount: Int = 0
+
+    private var sslContext: NIOOpenSSL.SSLContext?
 
     /// Should HTTP/2 protocol be used
     private var useHTTP2 = false
@@ -60,7 +72,9 @@ public class ClientRequest {
     }
 
 
-    init(url: String, callback: @escaping Callback) {
+   convenience init(url: String, callback: @escaping Callback) {
+        let options =  ClientRequest.parse(URL(string: url)!)
+        self.init(options: options, callback: callback)
         self.url = url
         self.callback = callback
     }
@@ -78,7 +92,7 @@ public class ClientRequest {
         case .maxRedirects(let maxRedirects):
             self.maxRedirects = maxRedirects
         case .disableSSLVerification:
-            self.disableSSLVerification = true
+            self.enableSSLVerification = true
         case .useHTTP2:
             self.useHTTP2 = true
         }
@@ -90,7 +104,7 @@ public class ClientRequest {
 
         var theSchema = "http://"
         var hostName = "localhost"
-        var path = ""
+        var path = "/"
         var port = ""
 
         for option in options  {
@@ -193,23 +207,35 @@ public class ClientRequest {
 
     var channel: Channel!
 
+    var bootstrap: ClientBootstrap!
+
     public func end(close: Bool = false) {
         //TODO: Handle redirection
         let group = MultiThreadedEventLoopGroup(numThreads: 1)
-        let bootstrap = ClientBootstrap(group: group)
-            .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-            .channelInitializer { channel in
-                channel.pipeline.addHTTPClientHandlers().then {
-                    channel.pipeline.add(handler: HTTPClientHandler(request: self))
-                }
-            }
+        if (URL(string: url)?.scheme)! == "https" {
+           enableSSLVerification = true
+        }
+
+        if enableSSLVerification {
+            initializeClientBootstrapWithSSL(eventLoopGroup: group)
+        } else {
+            initializeClientBootstrap(eventLoopGroup: group)
+        }
+
         let hostName = URL(string: url)?.host ?? "" //TODO: what could be the failure path here
+        if self.headers["Host"] == nil {
+           self.headers["Host"] = hostName
+        }
+        self.headers["User-Agent"] = "KituraNIO"
+        if self.port == nil {
+            self.port = enableSSLVerification ? 443 : 80
+        }
         do {
             channel = try bootstrap.connect(host: hostName, port: Int(self.port ?? 80)).wait()
         } catch {
             callback(nil)
             return //we must ideally throw from here, but alas!
-        } 
+        }
         var request = HTTPRequestHead(version: HTTPVersion(major: 1, minor:1), method: HTTPMethod.method(from: self.method), uri: self.path)
         request.headers = HTTPHeaders.from(dictionary: self.headers)
         channel.write(NIOAny(HTTPClientRequestPart.head(request)), promise: nil)
@@ -219,6 +245,31 @@ public class ClientRequest {
             channel.write(NIOAny(HTTPClientRequestPart.body(.byteBuffer(buffer))), promise: nil)
         }
         try! channel.writeAndFlush(NIOAny(HTTPClientRequestPart.end(nil))).wait()
+    }
+
+    private func initializeClientBootstrapWithSSL(eventLoopGroup: EventLoopGroup) {
+        if let sslConfig = self.sslConfig {
+            sslContext = try! SSLContext(configuration: sslConfig)
+        }
+        bootstrap = ClientBootstrap(group: eventLoopGroup)
+            .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+            .channelInitializer { channel in
+                channel.pipeline.add(handler: try! OpenSSLClientHandler(context: self.sslContext!)).then {
+                    channel.pipeline.addHTTPClientHandlers().then {
+                        channel.pipeline.add(handler: HTTPClientHandler(request: self))
+                    }
+                }
+            }
+    }
+
+    private func initializeClientBootstrap(eventLoopGroup: EventLoopGroup) {
+        bootstrap = ClientBootstrap(group: eventLoopGroup)
+            .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+            .channelInitializer { channel in
+                channel.pipeline.addHTTPClientHandlers().then {
+                    channel.pipeline.add(handler: HTTPClientHandler(request: self))
+                }
+            }
     }
 }
 
@@ -336,13 +387,22 @@ public class HTTPClientHandler: ChannelInboundHandler {
                  clientResponse.buffer!.write(buffer: &buffer)
              }
          case .end(_):
-            if clientResponse.statusCode == .movedTemporarily {
-                guard let url = clientResponse.headers["Location"]?.first else { fatalError("Redirected but no Location header") }
-                if url.starts(with: "/") {
-                    ClientRequest(options: [.hostname(clientRequest.hostName!), .port(clientRequest.port!), .path(url)], callback: clientRequest.callback)
-                        .end()
+            if clientResponse.statusCode == .movedTemporarily || clientResponse.statusCode == .movedPermanently {
+                self.clientRequest.redirectCount += 1
+                if self.clientRequest.redirectCount < self.clientRequest.maxRedirects {
+                    guard let url = clientResponse.headers["Location"]?.first else { fatalError("Redirected but no Location header") }
+                    if url.starts(with: "/") {
+                        let scheme = URL(string: clientRequest.url)?.scheme
+                        let request = ClientRequest(options: [.schema(scheme!), .hostname(clientRequest.hostName!), .port(clientRequest.port!), .path(url)], callback: clientRequest.callback)
+                        request.maxRedirects = self.clientRequest.maxRedirects - 1
+                        request.end()
+                    } else {
+                        let request = ClientRequest(url: url, callback: clientRequest.callback)
+                        request.maxRedirects = self.clientRequest.maxRedirects - 1
+                        request.end()
+                    }
                 } else {
-                    ClientRequest(url: url, callback: clientRequest.callback).end()
+                    clientRequest.callback(clientResponse)
                 }
             } else {
                 clientRequest.callback(clientResponse)
