Kitura/Kitura-NIO

commit 997a378e0db67cefe9a92103cd7c39fa0d6a84ab

Lazily translate HTTPHeaders to HeadersContainer[WIP] (#33)
* Lazily translate HTTPHeaders to HeadersContainer

* Remove duplicate append to HTTPHeaders

* test: HTTPHeaders matches HeadersContainer

Sources/KituraNIO/ClientResponse.swift
@@ -15,6 +15,7 @@
  */
 
 import NIO
+import NIOHTTP1
 import Foundation
 
 /// This class describes the response sent by the remote server to an HTTP request
@@ -36,9 +37,21 @@ public class ClientResponse {
     /// Minor version of HTTP of the response
     public var httpVersionMinor: UInt16? 
     
-    /// Set of HTTP headers of the response.
-    public var headers: HeadersContainer!
+    internal var _headers: HTTPHeaders!
 
+    /// Set of HTTP headers of the response.
+    public var headers: HeadersContainer {
+        get {
+            guard let httpHeaders = _headers else {
+                return HeadersContainer()
+            }
+            return HeadersContainer.create(from: httpHeaders)
+        }
+
+        set {
+           _headers = newValue.httpHeaders
+        }
+    }
     /// The HTTP Status code, as an Int, sent in the response by the remote server.
     public internal(set) var status = -1 {
         didSet {

Sources/KituraNIO/HTTP/HTTPServerResponse.swift
@@ -47,6 +47,8 @@ public class HTTPServerResponse: ServerResponse {
     /// The HTTP headers to be sent to the client as part of the response.
     public var headers : HeadersContainer = HeadersContainer()
 
+    private var _headers: HTTPHeaders = HTTPHeaders()
+
     /// The HTTP version to be sent in the response.
     private var httpVersion: HTTPVersion
 
@@ -103,7 +105,7 @@ public class HTTPServerResponse: ServerResponse {
             }
         }
 
-        let response = HTTPResponseHead(version: httpVersion, status: status, headers: headers.httpHeaders())
+        let response = HTTPResponseHead(version: httpVersion, status: status, headers: headers.httpHeaders)
         ctx.write(handler.wrapOutboundOut(.head(response)), promise: nil)
         if let buffer = buffer {
             ctx.write(handler.wrapOutboundOut(.body(.byteBuffer(buffer))), promise: nil)
@@ -122,8 +124,8 @@ public class HTTPServerResponse: ServerResponse {
         let status = HTTPResponseStatus(statusCode: errorCode.rawValue)
 
         //We don't keep the connection alive on an HTTP error
-        headers["Connection"] = ["Close"]
-        let response = HTTPResponseHead(version: HTTPVersion(major: 1, minor: 1), status: status, headers: headers.httpHeaders())
+        _headers.add(name: "Connection", value: "Close")
+        let response = HTTPResponseHead(version: HTTPVersion(major: 1, minor: 1), status: status, headers: _headers)
         ctx.write(handler.wrapOutboundOut(.head(response)), promise: nil)
         ctx.writeAndFlush(handler.wrapOutboundOut(.end(nil)), promise: nil)
         handler.updateKeepAliveState()

Sources/KituraNIO/HTTP/HeadersContainer.swift
@@ -26,6 +26,9 @@ public class HeadersContainer {
     /// The header storage
     internal var headers: [String: (key: String, value: [String])] = [:]
     
+    /// Will be used only by ServerResponse
+    internal var httpHeaders = HTTPHeaders()
+
     /// Create an instance of `HeadersContainer`
     public init() {}
 
@@ -43,9 +46,11 @@ public class HeadersContainer {
         set(newValue) {
             if let newValue = newValue {
                 set(key, value: newValue)
+                httpHeaders.replaceOrAdd(name: key, values: newValue)
             }
             else {
                 remove(key)
+                httpHeaders.remove(name: key)
             }
         }
     }
@@ -67,6 +72,7 @@ public class HeadersContainer {
             } else {
                 set(key, lowerCaseKey: lowerCaseKey, value: value)
             }
+            httpHeaders.add(name: key, values: value)
             
         case "content-type", "content-length", "user-agent", "referer", "host",
              "authorization", "proxy-authorization", "if-modified-since",
@@ -81,10 +87,12 @@ public class HeadersContainer {
         default:
             guard let oldValue = entry?.value.first else {
                 set(key, lowerCaseKey: lowerCaseKey, value: value)
+                httpHeaders.add(name: key, values: value)
                 return
             }
             let newValue = oldValue + ", " + value.joined(separator: ", ")
             headers[lowerCaseKey]?.value[0] = newValue
+            httpHeaders.replaceOrAdd(name: key, value: newValue)
         }
     }
 
@@ -103,6 +111,7 @@ public class HeadersContainer {
     /// Remove all of the headers
     public func removeAll() {
         headers.removeAll(keepingCapacity: true)
+        httpHeaders = HTTPHeaders()
     }
     
     private func set(_ key: String, value: [String]) {
@@ -118,6 +127,19 @@ public class HeadersContainer {
     }
 }
 
+extension HTTPHeaders {
+    mutating func add(name: String, values: [String]) {
+        values.forEach {
+            self.add(name: name, value: $0)
+        }
+    }
+
+    mutating func replaceOrAdd(name: String, values: [String]) {
+        values.forEach {
+            replaceOrAdd(name: name, value: $0)
+        }
+    }
+}
 /// Conformance to the `Collection` protocol
 extension HeadersContainer: Collection {
 
@@ -153,17 +175,6 @@ extension HeadersContainer: Collection {
 
 /// Kitura uses HeadersContainer and NIOHTTP1 expects HTTPHeader - bridging methods
 extension HeadersContainer {
-    /// HeadersContainer to HTTPHeaders
-    func httpHeaders() -> HTTPHeaders {
-        var httpHeaders = HTTPHeaders()
-        for (_, keyValues) in headers {
-            for value in keyValues.1 {
-                httpHeaders.add(name: keyValues.0, value: value)
-            }
-        }
-        return httpHeaders
-    }
-
     /// HTTPHeaders to HeadersContainer
     static func create(from httpHeaders: HTTPHeaders) -> HeadersContainer {
         let headerContainer = HeadersContainer()

Tests/KituraNIOTests/HTTPResponseTests.swift
@@ -58,4 +58,27 @@ class HTTPResponseTests: KituraNetTest {
         headers.append("Content-Type", value: "image/png, text/plain")
         XCTAssertEqual(headers["Content-Type"]?[0], "text/html")
     }
+
+    func testHeadersContainerHTTPHeaders() {
+        let headers = HeadersContainer()
+        headers["Content-Type"] = ["image/png, text/plain"]
+        XCTAssertEqual(headers.httpHeaders["Content-Type"], headers["Content-Type"])
+        headers["Content-Type"] = ["text/html"]
+        XCTAssertEqual(headers.httpHeaders["Content-Type"], headers["Content-Type"])
+        headers["Content-Type"] = nil
+        XCTAssertFalse(headers.httpHeaders.contains(name: "Content-Type"))
+        headers["Set-Cookie"] = ["ID=123BAS; Path=/; Secure; HttpOnly"]
+        headers.append("Set-Cookie", value: ["ID=KI9H12; Path=/; Secure; HttpOnly"])
+        XCTAssertEqual(headers["Set-Cookie"], headers.httpHeaders["Set-Cookie"])
+        headers["Content-Type"] = ["text/html"]
+        headers.append("Content-Type", value: "text/json")
+        XCTAssertEqual(headers.httpHeaders["Content-Type"], headers["Content-Type"])
+        headers["foo"] = ["bar0"]
+        headers.append("foo", value: "bar1")
+        XCTAssertEqual(headers.httpHeaders["foo"], headers["foo"])
+        headers.append("foo", value: ["bar2", "bar3"])
+        XCTAssertEqual(headers.httpHeaders["foo"], headers["foo"])
+        headers.removeAll()
+        XCTAssertFalse(headers.httpHeaders.contains(name: "foo"))
+    }
 }

Sources/KituraNIO/ClientRequest.swift
@@ -525,7 +525,7 @@ class HTTPClientHandler: ChannelInboundHandler {
          let request = self.unwrapInboundIn(data)
          switch request {
          case .head(let header):
-             clientResponse.headers = HeadersContainer.create(from: header.headers)
+             clientResponse._headers = header.headers
              clientResponse.httpVersionMajor = header.version.major
              clientResponse.httpVersionMinor = header.version.minor
              clientResponse.statusCode = HTTPStatusCode(rawValue: Int(header.status.code))!

Sources/KituraNIO/HTTP/HTTPServerRequest.swift
@@ -24,7 +24,11 @@ import Foundation
 public class HTTPServerRequest: ServerRequest {
 
     /// Set of HTTP headers of the request.
-    public var headers : HeadersContainer
+    public var headers : HeadersContainer {
+        return HeadersContainer.create(from: _headers)
+    }
+
+    private var _headers: HTTPHeaders
 
     @available(*, deprecated, message: "This contains just the path and query parameters starting with '/'. use 'urlURL' instead")
     public var urlString : String {
@@ -56,7 +60,7 @@ public class HTTPServerRequest: ServerRequest {
 
         self.enableSSL ? url.append("https://") : url.append("http://")
 
-        if let hostname = headers["Host"]?.first {
+        if let hostname = _headers["Host"].first {
             url.append(hostname)
             if !hostname.contains(":") {
                 url.append(":")
@@ -118,7 +122,7 @@ public class HTTPServerRequest: ServerRequest {
     }
 
     init(ctx: ChannelHandlerContext, requestHead: HTTPRequestHead, enableSSL: Bool) {
-        self.headers = HeadersContainer.create(from: requestHead.headers)
+        self._headers = requestHead.headers
         self.method = String(describing: requestHead.method)
         self.httpVersionMajor = requestHead.version.major
         self.httpVersionMinor = requestHead.version.minor

commit 5c28c59f6b267c0647c946427af2c0063baa7196

Fix a perf regression caused by PR #33

Sources/KituraNIO/ClientResponse.swift
@@ -49,7 +49,7 @@ public class ClientResponse {
         }
 
         set {
-           _headers = newValue.httpHeaders
+           _headers = newValue.httpHeaders()
         }
     }
     /// The HTTP Status code, as an Int, sent in the response by the remote server.

Sources/KituraNIO/HTTP/HTTPServerResponse.swift
@@ -47,7 +47,6 @@ public class HTTPServerResponse: ServerResponse {
     /// The HTTP headers to be sent to the client as part of the response.
     public var headers : HeadersContainer = HeadersContainer()
 
-    private var _headers: HTTPHeaders = HTTPHeaders()
 
     /// The HTTP version to be sent in the response.
     private var httpVersion: HTTPVersion
@@ -105,7 +104,7 @@ public class HTTPServerResponse: ServerResponse {
             }
         }
 
-        let response = HTTPResponseHead(version: httpVersion, status: status, headers: headers.httpHeaders)
+        let response = HTTPResponseHead(version: httpVersion, status: status, headers: headers.httpHeaders())
         ctx.write(handler.wrapOutboundOut(.head(response)), promise: nil)
         if let buffer = buffer {
             ctx.write(handler.wrapOutboundOut(.body(.byteBuffer(buffer))), promise: nil)
@@ -114,7 +113,7 @@ public class HTTPServerResponse: ServerResponse {
         handler.updateKeepAliveState()
 
         if let request = handler.serverRequest {
-                Monitor.delegate?.finished(request: request, response: self)
+            Monitor.delegate?.finished(request: request, response: self)
         }
     }
 
@@ -124,8 +123,8 @@ public class HTTPServerResponse: ServerResponse {
         let status = HTTPResponseStatus(statusCode: errorCode.rawValue)
 
         //We don't keep the connection alive on an HTTP error
-        _headers.add(name: "Connection", value: "Close")
-        let response = HTTPResponseHead(version: HTTPVersion(major: 1, minor: 1), status: status, headers: _headers)
+        headers["Connection"] = ["Close"]
+        let response = HTTPResponseHead(version: HTTPVersion(major: 1, minor: 1), status: status, headers: headers.httpHeaders())
         ctx.write(handler.wrapOutboundOut(.head(response)), promise: nil)
         ctx.writeAndFlush(handler.wrapOutboundOut(.end(nil)), promise: nil)
         handler.updateKeepAliveState()

Sources/KituraNIO/HTTP/HeadersContainer.swift
@@ -26,9 +26,6 @@ public class HeadersContainer {
     /// The header storage
     internal var headers: [String: (key: String, value: [String])] = [:]
     
-    /// Will be used only by ServerResponse
-    internal var httpHeaders = HTTPHeaders()
-
     /// Create an instance of `HeadersContainer`
     public init() {}
 
@@ -46,11 +43,9 @@ public class HeadersContainer {
         set(newValue) {
             if let newValue = newValue {
                 set(key, value: newValue)
-                httpHeaders.replaceOrAdd(name: key, values: newValue)
             }
             else {
                 remove(key)
-                httpHeaders.remove(name: key)
             }
         }
     }
@@ -72,7 +67,6 @@ public class HeadersContainer {
             } else {
                 set(key, lowerCaseKey: lowerCaseKey, value: value)
             }
-            httpHeaders.add(name: key, values: value)
             
         case "content-type", "content-length", "user-agent", "referer", "host",
              "authorization", "proxy-authorization", "if-modified-since",
@@ -87,12 +81,10 @@ public class HeadersContainer {
         default:
             guard let oldValue = entry?.value.first else {
                 set(key, lowerCaseKey: lowerCaseKey, value: value)
-                httpHeaders.add(name: key, values: value)
                 return
             }
             let newValue = oldValue + ", " + value.joined(separator: ", ")
             headers[lowerCaseKey]?.value[0] = newValue
-            httpHeaders.replaceOrAdd(name: key, value: newValue)
         }
     }
 
@@ -111,7 +103,6 @@ public class HeadersContainer {
     /// Remove all of the headers
     public func removeAll() {
         headers.removeAll(keepingCapacity: true)
-        httpHeaders = HTTPHeaders()
     }
     
     private func set(_ key: String, value: [String]) {
@@ -182,6 +173,16 @@ extension HeadersContainer {
             headerContainer.append(header.name, value: header.value)
         }
         return headerContainer
+    }
+
+    func httpHeaders() -> HTTPHeaders {
+        var httpHeaders = HTTPHeaders()
+        for (_, keyValues) in headers {
+            for value in keyValues.1 {
+                httpHeaders.add(name: keyValues.0, value: value)
+            }
+        }
+        return httpHeaders
     }
 }
 

Tests/KituraNIOTests/HTTPResponseTests.swift
@@ -62,23 +62,23 @@ class HTTPResponseTests: KituraNetTest {
     func testHeadersContainerHTTPHeaders() {
         let headers = HeadersContainer()
         headers["Content-Type"] = ["image/png, text/plain"]
-        XCTAssertEqual(headers.httpHeaders["Content-Type"], headers["Content-Type"])
+        XCTAssertEqual(headers.httpHeaders()["Content-Type"], headers["Content-Type"]!)
         headers["Content-Type"] = ["text/html"]
-        XCTAssertEqual(headers.httpHeaders["Content-Type"], headers["Content-Type"])
+        XCTAssertEqual(headers.httpHeaders()["Content-Type"], headers["Content-Type"]!)
         headers["Content-Type"] = nil
-        XCTAssertFalse(headers.httpHeaders.contains(name: "Content-Type"))
+        XCTAssertFalse(headers.httpHeaders().contains(name: "Content-Type"))
         headers["Set-Cookie"] = ["ID=123BAS; Path=/; Secure; HttpOnly"]
         headers.append("Set-Cookie", value: ["ID=KI9H12; Path=/; Secure; HttpOnly"])
-        XCTAssertEqual(headers["Set-Cookie"], headers.httpHeaders["Set-Cookie"])
+        XCTAssertEqual(headers["Set-Cookie"]!, headers.httpHeaders()["Set-Cookie"])
         headers["Content-Type"] = ["text/html"]
         headers.append("Content-Type", value: "text/json")
-        XCTAssertEqual(headers.httpHeaders["Content-Type"], headers["Content-Type"])
+        XCTAssertEqual(headers.httpHeaders()["Content-Type"], headers["Content-Type"]!)
         headers["foo"] = ["bar0"]
         headers.append("foo", value: "bar1")
-        XCTAssertEqual(headers.httpHeaders["foo"], headers["foo"])
+        XCTAssertEqual(headers.httpHeaders()["foo"], headers["foo"]!)
         headers.append("foo", value: ["bar2", "bar3"])
-        XCTAssertEqual(headers.httpHeaders["foo"], headers["foo"])
+        XCTAssertEqual(headers.httpHeaders()["foo"], headers["foo"]!)
         headers.removeAll()
-        XCTAssertFalse(headers.httpHeaders.contains(name: "foo"))
+        XCTAssertFalse(headers.httpHeaders().contains(name: "foo"))
     }
 }