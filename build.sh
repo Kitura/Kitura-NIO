65cd57b0c16faba06e49213ad90863574a11ca2e
Nethra Ravindran <nethra.ravindran@in.ibm.com>
2018-08-21 14:58:51 +0530

    Install pkg-config on docker

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ set -o verbose
 
 if [ -n "${DOCKER_IMAGE}" ]; then
     docker pull ${DOCKER_IMAGE}
-    docker run --privileged --env SWIFT_SNAPSHOT -v ${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE} /bin/bash -c "apt-get update && apt-get install -y git sudo lsb-release wget libxml2 && cd $TRAVIS_BUILD_DIR && ./build.sh"
+    docker run --privileged --env SWIFT_SNAPSHOT -v ${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE} /bin/bash -c "apt-get update && apt-get install -y git sudo lsb-release wget libxml2 pkg-config && cd $TRAVIS_BUILD_DIR && ./build.sh"
 else
-    sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
     git clone https://github.com/IBM-Swift/Package-Builder.git
     ./Package-Builder/build-package.sh -projectDir $(pwd)
 fi
