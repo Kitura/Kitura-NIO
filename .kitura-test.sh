d2fd3435eb4c083f0bc336d118ecce078aee2295
David Jones <djones6@uk.ibm.com>
2019-02-06 13:48:48 +0000

    Remove error handling, use Travis folding functions
    Ensure test failures are surfaced for backtrace generation

--- .kitura-test.sh
+++ .kitura-test.sh
@@ -1,41 +1,44 @@
 # Run Kitura-NIO tests
+travis_start "swift_test"
+echo ">> Executing Kitura-NIO tests"
 swift test
-echo ">> Done running tests. Now preparing to run Kitura tests ..."
+SWIFT_TEST_STATUS=$?
+travis_end
+if [ $SWIFT_TEST_STATUS -ne 0 ]; then
+  echo ">> swift test command exited with $SWIFT_TEST_STATUS"
+  # Return a non-zero status so that Package-Builder will generate a backtrace
+  return $SWIFT_TEST_STATUS
+fi
 
 # Clone Kitura
-echo ">> cd .. && git clone https://github.com/IBM-Swift/Kitura && cd Kitura"
+set -e
+echo ">> Building Kitura"
+travis_start "swift_build_kitura"
 cd .. && git clone https://github.com/IBM-Swift/Kitura && cd Kitura
 
 # Set KITURA_NIO
-echo ">> export KITURA_NIO=1"
 export KITURA_NIO=1
 
 # Build once
-echo ">> swift build"
 swift build
 
 # Edit package Kitura-NIO to point to the current branch
-echo ">> swift package edit Kitura-NIO --path ../Kitura-NIO"
+echo ">> Editing Kitura package to use latest Kitura-NIO"
 swift package edit Kitura-NIO --path ../Kitura-NIO
-echo ">> swift package edit returned $?."
-
-# If the `swift package edit` command failed, exit with the same failure code
-PACKAGE_EDIT_RESULT=$?
-if [[ $PACKAGE_EDIT_RESULT != 0 ]]; then
-    echo ">> Failed to edit the Kitura-NIO dependency."
-    exit $PACKAGE_EDIT_RESULT
-fi
+travis_end
+set +e
 
 # Run Kitura tests
-echo ">> swift test"
+travis_start "swift_test_kitura"
+echo ">> Executing Kitura tests"
 swift test
-
-# If the tests failed, exit
-TEST_EXIT_CODE=$?
-if [[ $TEST_EXIT_CODE != 0 ]]; then
-    exit $TEST_EXIT_CODE
+SWIFT_TEST_STATUS=$?
+travis_end
+if [ $SWIFT_TEST_STATUS -ne 0 ]; then
+  echo ">> swift test command exited with $SWIFT_TEST_STATUS"
+  # Return a non-zero status so that Package-Builder will generate a backtrace
+  return $SWIFT_TEST_STATUS
 fi
-echo ">> Done running Kitura tests."
 
 # Move back to the original build directory. This is needed on macOS builds for the subsequent swiftlint step.
 cd ../Kitura-NIO
