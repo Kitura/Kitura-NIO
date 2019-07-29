86657ae9970ff80077a0ae28056a15e56ec20632
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2019-07-29 20:37:57 +0530

    Remove unnecessary comma

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/BlueSSLService.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.3"),
+        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
