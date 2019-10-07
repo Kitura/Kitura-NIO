19e0c6bb70c80cf47ce6c77f33bb1f654b3f5d19
David Jones <djones6@uk.ibm.com>
2019-10-07 13:51:08 +0100

    Synchronise ServerOptions with Kitura-net

--- Sources/KituraNet/HTTP/ServerOptions.swift
+++ Sources/KituraNet/HTTP/ServerOptions.swift
@@ -19,9 +19,12 @@ import LoggerAPI
 
 /**
  ServerOptions allows customization of default server policies, including:
- - `requestSizeLimit`: Defines the maximum size of an incoming request, in bytes. If requests are received that are larger than this limit, they will be rejected and the connection will be closed. A value of `nil` means no limit.
+
+ - `requestSizeLimit`: Defines the maximum size for the body of an incoming request, in bytes. If a request body is larger than this limit, it will be rejected and the connection will be closed. A value of `nil` means no limit.
  - `connectionLimit`: Defines the maximum number of concurrent connections that a server should accept. Clients attempting to connect when this limit has been reached will be rejected. A value of `nil` means no limit.
+
  The server can optionally respond to the client with a message in either of these cases. This message can be customized by defining `requestSizeResponseGenerator` and `connectionResponseGenerator`.
+
  Example usage:
  ```
  let server = HTTP.createServer()
@@ -29,8 +32,8 @@ import LoggerAPI
  ```
  */
 public struct ServerOptions {
-    
-    /// A default limit of 100mb on the size of requests that a server should accept.
+
+    /// A default limit of 100mb on the size of the request body that a server should accept.
     public static let defaultRequestSizeLimit = 104857600
 
     /// A default limit of 10,000 on the number of concurrent connections that a server should accept.
@@ -50,52 +53,56 @@ public struct ServerOptions {
         return (.serviceUnavailable, "")
     }
 
-    /// Defines the maximum size of an incoming request, in bytes. If requests are received that are larger
-    /// than this limit, they will be rejected and the connection will be closed.
+    /// Defines the maximum size for the body of an incoming request, in bytes. If a request body is larger
+    /// than this limit, it will be rejected and the connection will be closed.
     ///
     /// A value of `nil` means no limit.
-    public var requestSizeLimit: Int?
+    public let requestSizeLimit: Int?
 
     /// Defines the maximum number of concurrent connections that a server should accept. Clients attempting
     /// to connect when this limit has been reached will be rejected.
-    public var connectionLimit: Int?
+    public let connectionLimit: Int?
 
     /**
      Determines the response message and HTTP status code used to respond to clients whose request exceeds
      the `requestSizeLimit`. The current limit and client's address are provided as parameters to enable a
      message to be logged, and/or a response to be provided back to the client.
+
      The returned tuple indicates the HTTP status code and response body to send to the client. If `nil` is
      returned, then no response will be sent.
+
      Example usage:
      ```
      let oversizeResponse: (Int, String) -> (HTTPStatusCode, String)? = { (limit, client) in
-     Log.debug("Rejecting request from \(client): Exceeds limit of \(limit) bytes")
-     return (.requestTooLong, "Your request exceeds the limit of \(limit) bytes.\r\n")
+         Log.debug("Rejecting request from \(client): Exceeds limit of \(limit) bytes")
+         return (.requestTooLong, "Your request exceeds the limit of \(limit) bytes.\r\n")
      }
      ```
      */
     public let requestSizeResponseGenerator: (Int, String) -> (HTTPStatusCode, String)?
-    
+
     /**
      Determines the response message and HTTP status code used to respond to clients that attempt to connect
      while the server is already servicing the maximum number of connections, as defined by `connectionLimit`.
      The current limit and client's address are provided as parameters to enable a  message to be logged,
      and/or a response to be provided back to the client.
+
      The returned tuple indicates the HTTP status code and response body to send to the client. If `nil` is
      returned, then no response will be sent.
+
      Example usage:
      ```
      let connectionResponse: (Int, String) -> (HTTPStatusCode, String)? = { (limit, client) in
-     Log.debug("Rejecting request from \(client): Connection limit \(limit) reached")
-     return (.serviceUnavailable, "Service busy - please try again later.\r\n")
+         Log.debug("Rejecting request from \(client): Connection limit \(limit) reached")
+         return (.serviceUnavailable, "Service busy - please try again later.\r\n")
      }
      ```
      */
     public let connectionResponseGenerator: (Int, String) -> (HTTPStatusCode, String)?
-    
+
     /// Create a `ServerOptions` to determine the behaviour of a `Server`.
     ///
-    /// - parameter requestSizeLimit: The maximum size of an incoming request. Defaults to `ServerOptions.defaultRequestSizeLimit`.
+    /// - parameter requestSizeLimit: The maximum size of an incoming request body. Defaults to `ServerOptions.defaultRequestSizeLimit`.
     /// - parameter connectionLimit: The maximum number of concurrent connections. Defaults to `ServerOptions.defaultConnectionLimit`.
     /// - parameter requestSizeResponseGenerator: A closure producing a response to send to a client when an over-sized request is rejected. Defaults to `ServerOptions.defaultRequestSizeResponseGenerator`.
     /// - parameter defaultConnectionResponseGenerator: A closure producing a response to send to a client when a the server is busy and new connections are not being accepted. Defaults to `ServerOptions.defaultConnectionResponseGenerator`.
