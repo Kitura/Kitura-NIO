ab04f11ae4ad43d97deeb60a0c97c034843745bf
Rudrani <rpwankh2@in.ibm.com>
2020-05-13 14:26:29 +0530

    implemetation of close and deinit

--- Sources/KituraNet/FastCGI/Server/FCGIConnector.swift
+++ Sources/KituraNet/FastCGI/Server/FCGIConnector.swift
@@ -1,69 +1,77 @@
-import Foundation
 import NIO
-import NIOHTTP1
-import NIOWebSocket
-import LoggerAPI
 import Foundation
-import Dispatch
 
-#if os(Linux)
-    let numberOfCores = Int(linux_sched_getaffinity())
-    fileprivate let globalELG = MultiThreadedEventLoopGroup(numberOfThreads: numberOfCores > 0 ? numberOfCores : System.coreCount)
-#else
-    fileprivate let globalELG = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-#endif
+public protocol FastCGIConnectorProtocol {
+    init(url: String)
 
-
-
-protocol FCGIConnectorProtocol {
-    
-    init(URL: String, keepAlive: Bool)
-   
-    func send( request: HTTPServerRequest, responseHandler: @escaping (HTTPResponseParts) -> Void)
+    func send(request: HTTPServerRequest,
+              keepAlive: Bool,
+              responseHandler: @escaping (HTTPResponseParts) -> Void) throws
+    func close()
 }
+
 public typealias HTTPResponseParts = (headers: [String: String], status: Int, body: Data?)
 
-public class FCGIConnector: FCGIConnectorProtocol {
-    var keepAlive: Bool
-    var bootstrap: ClientBootstrap?
-    var channel: Channel?
-    var responseReceived = DispatchSemaphore(value: 0)
-    var port: Int?
-    
-    public private(set) var url: String = ""
+public class FastCGIConnector: FastCGIConnectorProtocol {
 
-    private var percentEncodedURL: String = ""
+    private var url: String
+    private var hostname: String? = nil
+    private var port: Int? = nil
+    private var channel: Channel? = nil
+    private var responseReceived = DispatchSemaphore(value: 0)
     
-    required init(URL: String, keepAlive: Bool = false) {
-        self.url = URL
-        self.keepAlive = keepAlive
-    }
-    
-    private func initializeClientBootstrap(eventLoopGroup: EventLoopGroup) {
-        bootstrap = ClientBootstrap(group: eventLoopGroup)
-            .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-            .channelInitializer { channel in
-                channel.pipeline.addHandler(RequestTranslator()).flatMap {_ in
-                    channel.pipeline.addHandler(FastCGIRecordEncoderHandler<FastCGIRecordEncoder>())
-                }
-            }
-    }
-    
-    func send(request: HTTPServerRequest, responseHandler: @escaping (HTTPResponseParts) -> Void) {
-        
-        let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        initializeClientBootstrap(eventLoopGroup: group)
-        let hostName = URL(string: percentEncodedURL)?.host ?? "" //TODO: what could be the failure path here
-        let portNumber = self.port
-        do {
-            guard let bootstrap = bootstrap else { return }
-            channel = try! bootstrap.connect(host: hostName, port: Int(self.port!)).wait() as Channel
-            try! self.channel?.writeAndFlush(request).wait()
-            responseReceived.wait()
-            if (keepAlive == false) {
-                self.channel?.close(promise: nil)
-            }
+    required public init (url: String) {
+        self.url = url
+        if let url = URL(string: url) {
+            self.hostname = url.host
+            self.port = url.port
         }
     }
-}
     
+    public func send(request: HTTPServerRequest,
+                     keepAlive: Bool,
+                     responseHandler: @escaping (HTTPResponseParts) -> Void) throws {
+        
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let bootstrap = ClientBootstrap(group: group)
+            .channelInitializer { channel in
+                channel.pipeline.addHandler(FastCGIRecordEncoderHandler<FastCGIRecordEncoder>()).flatMap { _ in channel.pipeline.addHandler(RequestTranslator())
+                }.flatMap { _ in channel.pipeline.addHandler(FastCGIRecordDecoderHandler<FastCGIRecordDecoder>())
+                }.flatMap { _ in
+                    channel.pipeline.addHandler(ResponseTranslator())
+                }.flatMap { _ in
+                    channel.pipeline.addHandler(FastCGIProxyHandler(responseHandler: responseHandler))
+                }
+        }
+        guard let host = self.hostname, let port = self.port else { return } //TODO: throw an error
+        self.channel = try! bootstrap.connect(host: host, port: port).wait()
+        try! self.channel?.writeAndFlush(request).wait()
+        responseReceived.wait()
+        
+        if keepAlive {
+            self.channel?.close(promise: nil)
+        }
+    }
+    public func close() {
+        self.channel?.close(promise: nil)
+        }
+
+    deinit {
+        self.channel?.close(promise: nil)
+        }
+    }
+
+class FastCGIProxyHandler: ChannelInboundHandler {
+    typealias InboundIn = HTTPResponseParts
+
+    let responseHandler: (HTTPResponseParts) -> Void
+
+    init(responseHandler: @escaping (HTTPResponseParts) -> Void) {
+        self.responseHandler = responseHandler
+    }
+
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        let responseParts = self.unwrapInboundIn(data)
+        responseHandler(responseParts)
+    }
+}
