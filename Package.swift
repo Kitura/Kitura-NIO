aac8daef647c4450524c422ab15ada507c3ff8b4
Danny Sung <danny@dannysung.com>
2020-10-03 16:06:43 -0700

    Update package reference to community versions

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.8.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/BlueSSLService.git", from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.7.3")
+        .package(url: "https://github.com/Kitura/BlueSSLService.git", from: "1.0.200"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.200")
     ],
     targets: [
         .target(
